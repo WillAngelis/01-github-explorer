8dea63e4bcc75407edded8f4073e75a0fa48600d
WillAngelis <willangelis@gmail.com>
2022-07-27 15:50:47 -0300

    add props to all components

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,10 @@
 import { RepositoryItem } from './RepositoryItem';
 
-const repositoryName = 'unform';
+const repository = {
+  name: 'unform',
+  description: 'form in React',
+  link: 'https://github.com/unform/unform',
+};
 
 export function RepositoryList() {
   return (
@@ -8,9 +12,9 @@ export function RepositoryList() {
       <h1>Lista de repositorios</h1>
 
       <ul>
-        <RepositoryItem repository="Repositorio 1" />
-        <RepositoryItem />
-        <RepositoryItem />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
